4f48f875e2d201e9c8f2d2da3f7f0a0e792fff9d
chrisbensen <69060304+chrisbensen@users.noreply.github.com>
2022-08-19 17:50:28 -0700

    Add twitter support variables

---
+++ source/cloud/settwitter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#source settwitter.sh
+
+export API_KEY=""
+export API_KEY_SECRET=""
+export ACCESS_TOKEN=""
+export ACCESS_TOKEN_SECRET=""
---
+++ source/cloud/unsettwitter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#source unsettwitter.sh
+
+unset API_KEY
+unset API_KEY_SECRET
+unset ACCESS_TOKEN
+unset ACCESS_TOKEN_SECRET
