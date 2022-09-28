56137e9ffb1b551676b5aa325c83fa150ca12c73
SergiyIvan <serhii.ivanenko@oracle.com>
2022-09-28 11:39:35 +0100

    Adding flag for choosing GV or python.

--- source/pi/warble.sh
+++ source/pi/warble.sh
@@ -4,6 +4,7 @@ USERNAME=$1
 CODE=$2
 URL=$3
 TWEET=$4
+MODE=$5
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 function check_environment {
@@ -27,23 +28,33 @@ function register_function {
 
 pushd ../warble
 
-check_environment
-GRAALVISOR_IP=127.0.0.1
-GRAALVISOR_PORT=8080
-# Checking if the default port of Graalvisor is in use.
-if [ -z "$(sudo lsof -i -P -n | grep LISTEN | grep $GRAALVISOR_PORT)" ]
+if [ "$MODE" == "gv" ]
 then
-  # Graalvisor is down, we have to launch it and register the function.
-  start_graalvisor &> /tmp/graalvisor.log &
-  sleep 1
-  register_function
+  check_environment
+  GRAALVISOR_IP=127.0.0.1
+  GRAALVISOR_PORT=8080
+
+  # Checking if the default port of Graalvisor is in use.
+  if [ -z "$(sudo lsof -i -P -n | grep LISTEN | grep $GRAALVISOR_PORT)" ]
+  then
+    # Graalvisor is down, we have to launch it and register the function.
+    start_graalvisor &> /tmp/graalvisor.log &
+    sleep 1
+    register_function
+  fi
+
+  # Prepare code for Graalvisor - escape double quotes correctly.
+  CODE=$(echo "$CODE" | sed 's/"/\\\\"/g')
+  CODE=$(echo "$CODE" | sed 's/"/\\"/g')
+  curl -s -X POST $GRAALVISOR_IP:$GRAALVISOR_PORT -H 'Content-Type: application/json' -d $'{"name":"warble","async":"false","arguments":"[\'-v\',\'--username\',\'\\"'$USERNAME$'\\"\',\''$CODE$'\']"}'
+elif [ -z "$MODE" ] || [ "$MODE" == "python" ]
+then
+  python3 warblecc.py --username ${USERNAME} ${CODE}
+else
+  echo "Only 'python' (default) and 'gv' modes are supported."
+  exit 1
 fi
 
-# Prepare code for Graalvisor - escape double quotes correctly.
-CODE=$(echo "$CODE" | sed 's/"/\\\\"/g')
-CODE=$(echo "$CODE" | sed 's/"/\\"/g')
-
-curl -s -X POST $GRAALVISOR_IP:$GRAALVISOR_PORT -H 'Content-Type: application/json' -d $'{"name":"warble","async":"false","arguments":"[\'-v\',\'--username\',\'\\"'$USERNAME$'\\"\',\''$CODE$'\']"}'
 PROGRAM="out.py"
 OUTPUT=$(python3 $PROGRAM)
 
