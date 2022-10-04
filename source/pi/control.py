1f496e0d37f255e3e960e83fd0429fdf716449e6
chrisbensen <69060304+chrisbensen@users.noreply.github.com>
2022-10-04 14:53:52 -0700

    remove timestamp

--- source/pi/control.py
+++ source/pi/control.py
@@ -237,7 +237,6 @@ def getInfo():
     #temperature = output.decode('utf-8').strip()
 
     # Output Info
-    result['time'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     result["CPU"] = cpu
     result["MemoryFree"] = memory_available
     result["MemoryTotal"] = memory_total
