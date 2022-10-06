71815045547d26599d4f8db2d7a27a26452bc939
chrisbensen <69060304+chrisbensen@users.noreply.github.com>
2022-10-06 11:09:40 -0700

    4

--- source/pi/warble.py
+++ source/pi/warble.py
@@ -7,36 +7,35 @@ import requests
 # Run: python3 warblec.py -v "{PRINT(\"test\")}"
 
 def main():
-    print("here")
-    # argparser = argparse.ArgumentParser(description='Pre Warble')
-    # argparser.add_argument('code', type=str, help='code')
-    # argparser.add_argument('-v', '--verbose', action='store_true')
-    # argparser.add_argument('-u', '--username', required=False, help='username')
-    # argparser.add_argument('-t', '--tweet', required=False, help='tweet')
-    # argparser.add_argument('-r', '--url', required=False, help='url')
-    # args = argparser.parse_args()
-    # input = args.code
-    # username = args.username
-    # url = args.url
-    # tweet = args.tweet
-    #
-    # if args.verbose:
-    #     utils.verbose = True
-    #
-    # stream = os.popen('python3 ../warblecc.py \"{}\" {} {}'.format(code, username))
-    # output = stream.read()
-    #
-    # try:
-    #     data = { "tweet": tweet, "code": code, "output": output }
-    #     print(data)
-    #     # headers = {'Content-type': 'application/json'}
-    #     # response = requests.post(url, data=json.dumps(data), headers=headers)
-    #     # message = response.json()
-    #     # if message["status"] == 'true':
-    #     #   print(data)
-    #
-    # except socket.error:
-    #     print("error")
+    argparser = argparse.ArgumentParser(description='Pre Warble')
+    argparser.add_argument('code', type=str, help='code')
+    argparser.add_argument('-v', '--verbose', action='store_true')
+    argparser.add_argument('-u', '--username', required=False, help='username')
+    argparser.add_argument('-t', '--tweet', required=False, help='tweet')
+    argparser.add_argument('-r', '--url', required=False, help='url')
+    args = argparser.parse_args()
+    input = args.code
+    username = args.username
+    url = args.url
+    tweet = args.tweet
+
+    if args.verbose:
+        utils.verbose = True
+
+    stream = os.popen('python3 ../warblecc.py {} {}'.format(code, username))
+    output = stream.read()
+
+    try:
+        data = { "tweet": tweet, "code": code, "output": output }
+        print(data)
+        # headers = {'Content-type': 'application/json'}
+        # response = requests.post(url, data=json.dumps(data), headers=headers)
+        # message = response.json()
+        # if message["status"] == 'true':
+        #   print(data)
+
+    except socket.error:
+        print("error")
 
 
 if __name__ == "__main__":
